5c39fa47be82edddeccf28d5c3d62b1dffcdbb86
Nguyễn Mạnh Cường <145275903+manhcuong17072002@users.noreply.github.com>
2025-01-15 05:19:16 +0300

    Update cleaner.py for Vietnamese

--- melo/text/cleaner.py
+++ melo/text/cleaner.py
@@ -1,9 +1,8 @@
-from . import chinese, japanese, english, chinese_mix, korean, french, spanish
+from . import english, vietnamese
 from . import cleaned_text_to_sequence
 import copy
 
-language_module_map = {"ZH": chinese, "JP": japanese, "EN": english, 'ZH_MIX_EN': chinese_mix, 'KR': korean,
-                    'FR': french, 'SP': spanish, 'ES': spanish}
+language_module_map = {"EN": english, "VI": vietnamese}
 
 
 def clean_text(text, language):
@@ -13,12 +12,16 @@ def clean_text(text, language):
     return norm_text, phones, tones, word2ph
 
 
-def clean_text_bert(text, language, device=None):
+def clean_text_bert(text, language, device="cpu"):
     language_module = language_module_map[language]
     norm_text = language_module.text_normalize(text)
     phones, tones, word2ph = language_module.g2p(norm_text)
     
+    # Create a deep copy of word2ph to preserve the original values
+    # This is necessary because the subsequent operations will modify word2ph
+    # copy.deepcopy() creates a completely independent copy of the object and all its nested objects
     word2ph_bak = copy.deepcopy(word2ph)
+    
     for i in range(len(word2ph)):
         word2ph[i] = word2ph[i] * 2
     word2ph[0] += 1
