3a859b4bcf764f927a75b3e1f27e8582d6072b20
Nguyễn Mạnh Cường <145275903+manhcuong17072002@users.noreply.github.com>
2025-01-15 04:54:29 +0300

    Update train.sh for training

--- melo/train.sh
+++ melo/train.sh
@@ -1,19 +1,27 @@
-CONFIG=$1
-GPUS=$2
+CONFIG=/storage2/melotts/dataset/infore/config.json
+DUMMY=/storage2/melotts/dataset
+GPUS=1
+G=/storage2/melotts/melo/logs/infore/G_4000.pth
+D=/storage2/melotts/melo/logs/infore/D_4000.pth
+DUR=/storage2/melotts/melo/logs/infore/DUR_4000.pth
 MODEL_NAME=$(basename "$(dirname $CONFIG)")
 
 PORT=10902
 
-while : # auto-resume: the code sometimes crash due to bug of gloo on some gpus
-do
-torchrun --nproc_per_node=$GPUS \
-        --master_port=$PORT \
-    train.py --c $CONFIG --model $MODEL_NAME 
+rm -r DUMMY
+ln -s $DUMMY DUMMY
 
-for PID in $(ps -aux | grep $CONFIG | grep python | awk '{print $2}')
-do
-    echo $PID
-    kill -9 $PID
-done
-sleep 30
-done
+# while : # auto-resume: the code sometimes crash due to bug of gloo on some gpus
+# do
+CUDA_VISIBLE_DEVICES=0 torchrun --nproc_per_node=$GPUS \
+        --master_port=$PORT \
+    train.py --c $CONFIG --model $MODEL_NAME
+    --pretrain_G $G --pretrain_D $D --pretrain_dur $DUR
+
+# for PID in $(ps -aux | grep $CONFIG | grep python | awk '{print $2}')
+# do
+#     echo $PID
+#     kill -9 $PID
+# done
+# sleep 30
+# done
