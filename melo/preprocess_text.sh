1fcc2068b39ea1511671557fbbd7428dbf8e5092
Nguyễn Mạnh Cường <77909327+manhcuong02@users.noreply.github.com>
2025-05-09 23:40:27 +0700

    Update preprocess_text.sh

--- melo/preprocess_text.sh
+++ melo/preprocess_text.sh
@@ -1,7 +1,7 @@
 
 python preprocess_text.py \
-    --metadata /home/misa/home/misa/MeloTTS_Vietnamese/dataset/NHLy/text_training.list \
-    --config_path /home/misa/home/misa/MeloTTS_Vietnamese/dataset/NHLy/config.json \
+    --metadata /path/to/text_training.list \
+    --config_path /path/to/config.json \
     --device cuda:0 \
     --val-per-spk 10 \
     --max-val-total 500
